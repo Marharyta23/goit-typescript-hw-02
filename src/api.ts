3ff57ab9cd91b9cf123356ba86a1a7ed2e8b1801
Marharyta23 <144152456+Marharyta23@users.noreply.github.com>
2024-04-26 14:12:53 +0300

    fix

--- src/api.ts
+++ src/api.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { Photo } from "./types";
 
 const API_KEY: string = "t0qyi3naDpSfQj9P4h4Tj3gZ8pTOAVGXJJPI0p0fPM8";
 
@@ -7,10 +8,15 @@ axios.defaults.params = {
   per_page: 15,
 };
 
-export const getPhotos = async (query: string, page: number) => {
+interface Data {
+  results: Photo[];
+  total: number;
+  total_pages: number;
+}
+
+export const getPhotos = async (query: string, page: number): Promise<Data> => {
   const { data } = await axios.get(
     `https://api.unsplash.com/search/photos?query=${query}&page=${page}&client_id=${API_KEY}`
   );
-
   return data;
 };
